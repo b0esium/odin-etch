f11deb039a5ef757bf1228679e0d68308320c0da
b0esium <b0esium@protonmail.com>
2022-10-26 15:38:52 +0200

    Draw fixed side centered static grid

---
+++ app.js
@@ -0,0 +1,14 @@
+let grid = document.getElementById("grid");
+
+const SQUARE_SIDE = 16;
+
+for (let column = 1; column <= SQUARE_SIDE; column++) {
+  let col = document.createElement("div");
+  col.setAttribute("class", "column");
+  grid.append(col);
+  for (let row = 1; row <= SQUARE_SIDE; row++) {
+    let cell = document.createElement("div");
+    cell.setAttribute("class", "cell");
+    col.append(cell);
+  }
+}
