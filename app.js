f2ac2a52d1ee6a033012a8b42abaed87cb9cc5a3
b0esium <b0esium@protonmail.com>
2022-10-27 11:43:23 +0200

    Keep fixed grid width and adapt cell size

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let size = 16;
 let grid = document.getElementById("grid");
+const gridWidth = 800;
+grid.setAttribute("width", `${gridWidth}px`);
 let body = document.querySelector("body");
 
 body.addEventListener("load", drawGrid(size));
@@ -12,12 +14,13 @@ function drawGrid(size) {
     for (let column = 1; column <= size; column++) {
       let cell = document.createElement("div");
       cell.setAttribute("class", "cell");
-      cell.setAttribute("style", "padding:8px");
+      let padding = (gridWidth - 2 * size) / (2 * size); // border = 1px * 2
+      cell.setAttribute("style", `padding:${padding}px`);
       cell.innerText = " ";
       cell.addEventListener("mouseenter", () => {
         cell.setAttribute("class", "on");
         cell.setAttribute("style", "padding:0");
-        cell.setAttribute("style", "width:18px");
+        cell.setAttribute("style", `width:${2 * padding + 2}px`); // border = 1px * 2
       });
       row.append(cell);
     }
