4128a4d28984c2b3ee9760889fe9e3077cac6bf8
b0esium <b0esium@protonmail.com>
2022-10-27 10:58:52 +0200

    Make button to change grid size

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
+let size = 16;
 let grid = document.getElementById("grid");
+let body = document.querySelector("body");
 
-const SQUARE_SIDE = 16;
+body.addEventListener("load", drawGrid(size));
 
-for (let column = 1; column <= SQUARE_SIDE; column++) {
-  let col = document.createElement("div");
-  col.setAttribute("class", "column");
-  grid.append(col);
-  for (let row = 1; row <= SQUARE_SIDE; row++) {
-    let cell = document.createElement("div");
-    cell.setAttribute("class", "cell");
-    cell.innerText = " ";
-    cell.addEventListener("mouseenter", () => {
-      cell.setAttribute("class", "on");
-    });
-    col.append(cell);
+function drawGrid(size) {
+  for (let row = 1; row <= size; row++) {
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    grid.append(row);
+    for (let column = 1; column <= size; column++) {
+      let cell = document.createElement("div");
+      cell.setAttribute("class", "cell");
+      cell.innerText = " ";
+      cell.addEventListener("mouseenter", () => {
+        cell.setAttribute("class", "on");
+      });
+      row.append(cell);
+    }
   }
 }
+
+function eraseGrid() {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+}
+
+// button to change size
+const changeSizeBtn = document.getElementById("changeSizeBtn");
+changeSizeBtn.addEventListener("click", () => {
+  newSize = prompt("New square side length?");
+  eraseGrid();
+  drawGrid(newSize);
+});
